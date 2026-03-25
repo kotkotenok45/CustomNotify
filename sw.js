68f4532372cb23f871249afa832d79d0b0571225
kotkotenok45 <kotkotenok43434343@gmail.com>
2026-03-25 22:44:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,23 @@
+const CACHE_NAME = 'custom-notify-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
 self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
     event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((res) => res || fetch(event.request))
+    );
 });
